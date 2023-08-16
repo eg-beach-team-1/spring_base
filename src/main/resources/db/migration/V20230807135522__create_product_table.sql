fdb2783b815dd0db6da423e2c35f55569444d118
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-16 10:03:10 +0800

    [TL-4-1][Chen] add discount field in product table

--- src/main/resources/db/migration/V20230807135522__create_product_table.sql
+++ src/main/resources/db/migration/V20230807135522__create_product_table.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS `product`
 (
     `id`                   INT            NOT NULL AUTO_INCREMENT COMMENT '主键',
     `name`                 VARCHAR(32)    NOT NULL                COMMENT '商品名称',
-    `price`                DECIMAL(19,2)                          COMMENT '价格',
+    `price`                DECIMAL(10,2)                          COMMENT '价格',
     `status`               VARCHAR(32)    NOT NULL                COMMENT '状态',
+    `discount`             DECIMAL(10,2)  NOT NULL                COMMENT '折扣',
 PRIMARY KEY(`id`)
 USING BTREE
 )ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
