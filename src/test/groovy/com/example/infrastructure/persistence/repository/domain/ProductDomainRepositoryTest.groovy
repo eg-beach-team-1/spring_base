4116961f0012ab224fe34c8bd34053c5cd083c3e
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-16 14:42:31 +0800

    [TL-4-2][Chen] modify ProductDomainRepositoryTest

--- src/test/groovy/com/example/infrastructure/persistence/repository/domain/ProductDomainRepositoryTest.groovy
+++ src/test/groovy/com/example/infrastructure/persistence/repository/domain/ProductDomainRepositoryTest.groovy
@@ -15,17 +15,17 @@ class ProductDomainRepositoryTest extends Specification {
     def "should return all products"() {
         given:
         List<ProductPo> jpaProducts = [
-                new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID"),
-                new ProductPo(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID"),
-                new ProductPo(id: 3, name: "book2", price: null, status: "VALID"),
+                new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID", discount: BigDecimal.ONE),
+                new ProductPo(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID", discount: BigDecimal.valueOf(0.8)),
+                new ProductPo(id: 3, name: "book2", price: null, status: "VALID", discount: BigDecimal.valueOf(0.5)),
         ]
 
         jpaProductRepository.findAll() >> jpaProducts
 
         List<Product> expectedProducts = [
-                new Product(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID"),
-                new Product(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID"),
-                new Product(id: 3, name: "book2", price: null, status: "VALID"),
+                new Product(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID", discount: BigDecimal.ONE),
+                new Product(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID", discount: BigDecimal.valueOf(0.8) ),
+                new Product(id: 3, name: "book2", price: null, status: "VALID", discount: BigDecimal.valueOf(0.5)),
         ]
 
         when:
@@ -40,17 +40,17 @@ class ProductDomainRepositoryTest extends Specification {
 
     def "should return correct product"() {
         given:
-        ProductPo jpaProduct = new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID")
+        ProductPo jpaProduct = new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID", discount: BigDecimal.valueOf(0.5))
 
         jpaProductRepository.findById(1) >> Optional.of(jpaProduct)
 
-        Product expectedProduct = new Product(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID")
+        Product expectedProduct = new Product(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID", discount: BigDecimal.valueOf(0.5))
 
         when:
         def result = productDomainRepository.findById(1)
 
         then:
-        Assertions.assertThat(result.equals(expectedProduct))
+        Assertions.assertThat(result == expectedProduct)
     }
 
 
@@ -68,27 +68,25 @@ class ProductDomainRepositoryTest extends Specification {
     def "should find all products by ids"() {
         given:
         List<ProductPo> jpaProducts = [
-                new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID"),
-                new ProductPo(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID"),
-                new ProductPo(id: 3, name: "book2", price: null, status: "VALID"),
+                new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID", discount: BigDecimal.ONE),
+                new ProductPo(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID", discount: BigDecimal.valueOf(0.8)),
+                new ProductPo(id: 3, name: "book2", price: null, status: "VALID", discount: BigDecimal.ONE),
         ]
 
-        jpaProductRepository.findAllById(_) >> jpaProducts
+        jpaProductRepository.findAllById(_ as Iterable<Integer>) >> jpaProducts
 
         List<Product> expectedProducts = [
-                new Product(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID"),
-                new Product(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID"),
-                new Product(id: 3, name: "book2", price: null, status: "VALID"),
-        ]
+                new ProductPo(id: 1, name: "book", price: BigDecimal.valueOf(10L), status: "VALID"),
+                new ProductPo(id: 2, name: "book2", price: BigDecimal.valueOf(10L), status: "INVALID", discount: BigDecimal.valueOf(0.8)),
+                new ProductPo(id: 3, name: "book2", price: null, status: "VALID", discount: BigDecimal.ONE),
+        ] as List<Product>
 
         when:
         def result = productDomainRepository.findAllById(List.of(1, 2, 3))
 
         then:
-        Assertions.assertThat(result)
-                .usingRecursiveComparison()
-                .ignoringCollectionOrder()
-                .isEqualTo(expectedProducts)
+        Assertions.assertThat(result == expectedProducts)
+
     }
 
 
