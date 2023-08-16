d1df93139c72d86c6d6c9fe1dc383d081f425b57
Weixiao <weixiao.chen.98@foxmail.com>
2023-08-16 11:35:16 +0800

    [TL-7-1][Weixiao] modify flyway script to add stock field into table product

--- src/main/resources/db/migration/V20230807135522__create_product_table.sql
+++ src/main/resources/db/migration/V20230807135522__create_product_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `product`
     `price`                DECIMAL(10,2)                          COMMENT '价格',
     `status`               VARCHAR(32)    NOT NULL                COMMENT '状态',
     `discount`             DECIMAL(10,2)  NOT NULL                COMMENT '折扣',
+    `stock`                INT            NOT NULL,
 PRIMARY KEY(`id`)
 USING BTREE
 )ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
