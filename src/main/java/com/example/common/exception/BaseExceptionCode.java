e65303ff10584d879c0a0e58219553a667dd6409
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-17 14:16:19 +0800

    [TL-8-3][Chen] add product stock shortage error code

--- src/main/java/com/example/common/exception/BaseExceptionCode.java
+++ src/main/java/com/example/common/exception/BaseExceptionCode.java
@@ -8,6 +8,8 @@ public enum BaseExceptionCode implements IExceptionCode {
   NOT_FOUND_PRODUCT("Not found product."),
 
   OUT_OF_STOCK("this product is out of stock."),
+
+  PRODUCT_STOCK_SHORTAGE("the stock of this product is less than the amount"),
   INVALID_PRODUCT("Invalid product.");
 
 
