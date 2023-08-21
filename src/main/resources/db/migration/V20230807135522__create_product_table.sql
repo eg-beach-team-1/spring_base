0555c814af323dc08763dd4bc5cf6cd2c9f5c51b
ChenZhuBuffalo <chen.zhu1@thoughtworks.com>
2023-08-21 11:20:58 +0800

    [TL-10-1][Chen] add version field in Product table

--- src/main/resources/db/migration/V20230807135522__create_product_table.sql
+++ src/main/resources/db/migration/V20230807135522__create_product_table.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS `product`
     `price`                DECIMAL(10,2)                          COMMENT '价格',
     `status`               VARCHAR(32)    NOT NULL                COMMENT '状态',
     `discount`             DECIMAL(10,2)  NOT NULL                COMMENT '折扣',
-    `stock`                INT            NOT NULL,
+    `stock`                INT            NOT NULL                COMMENT '库存',
+    'version'              INT            NOT NULL                COMMENT '版本',
 PRIMARY KEY(`id`)
 USING BTREE
 )ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
