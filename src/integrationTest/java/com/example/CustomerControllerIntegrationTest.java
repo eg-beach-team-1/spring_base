592791fcecdcafffa4524dffc0636311f368b765
Zhongwen Lian <lianzhongwen@gmail.com>
2023-08-23 11:18:26 +0800

    [17-8][Zhongwen] throw 404 user not found is user is not found

--- src/integrationTest/java/com/example/CustomerControllerIntegrationTest.java
+++ src/integrationTest/java/com/example/CustomerControllerIntegrationTest.java
@@ -2,6 +2,8 @@ package com.example;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
@@ -15,8 +17,20 @@ public class CustomerControllerIntegrationTest extends BaseIntegrationTest {
         .when()
         .get("/customers/{id}", "1")
         .then()
-        .statusCode(200)
+        .statusCode(OK.value())
         .body("id", equalTo("1"))
         .body("name", equalTo("client"));
+  }
+
+  @Test
+  @DataSet("retrieve_customer_by_id_successfully.yml")
+  public void retrieve_throw_404_is_user_not_found() {
+    given()
+        .when()
+        .get("/customers/{id}", "2")
+        .then()
+        .statusCode(NOT_FOUND.value())
+        .body("code", equalTo("NOT_FOUND_CUSTOMER"))
+        .body("message", equalTo("Not found customer."));
   }
 }
