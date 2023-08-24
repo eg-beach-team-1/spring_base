6818aa8fe144ff159289a4fe8259cfd2f87e6a22
Weixiao <weixiao.chen.98@foxmail.com>
2023-08-24 16:06:50 +0800

    [Tech][Weixiao] add product sample data

---
+++ src/main/resources/db/migration/V20230824160622__insert_product_sample_data.sql
@@ -0,0 +1,6 @@
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (1, 'product-1', 10.00, 'VALID', 1.0, 10);
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (2, 'product-2', 10.00, 'VALID', 0.2, 10);
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (3, 'product-3', 10.00, 'VALID', 0.3, 10);
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (4, 'product-4', 10.00, 'VALID', 1.0, 10);
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (5, 'product-5', 10.00, 'INVALID', 1.0, 10);
+insert into product(`id`, `name`, `price`, `status`, `discount`, `stock`) values (6, 'product-6', 10.00, 'INVALID', 1.0, 10);
