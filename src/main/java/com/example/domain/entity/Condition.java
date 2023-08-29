b1e51432276a29b93c7f0e147e02ec542e94bb79
Linhao Jia <252620356@qq.com>
2023-08-29 15:04:12 +0800

    [21][Linhao & Weixiao] create Condition abstract class with isSatisfied func

---
+++ src/main/java/com/example/domain/entity/Condition.java
@@ -0,0 +1,12 @@
+package com.example.domain.entity;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public abstract class Condition {
+  protected BigDecimal discount;
+
+  public abstract boolean isSatisfied(Map<Integer, Integer> productIdToQuantity);
+}
