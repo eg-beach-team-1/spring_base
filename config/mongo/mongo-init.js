813b9dddc43e7e3e817029b52fb73600c3da0efa
Zhongwen Lian <lianzhongwen@gmail.com>
2023-08-29 11:22:48 +0800

    Revert "[27][Zhongwen & Junyi & Hongbo] Init mongodb setup"
    This reverts commit 5d78464a4e219a29bf268e8d818f55063f0cfe91.

--- config/mongo/mongo-init.js
+++
@@ -1,9 +0,0 @@
-use spring_base;
-
-db.createUser({
-    user: 'normal_user',
-    pwd: 'normal_password',
-    roles: [{ role: 'readWrite', db: 'spring_base'}]
-});
-
-db.createCollection('discount');
