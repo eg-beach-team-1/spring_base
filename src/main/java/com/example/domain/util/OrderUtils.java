1676740f09e0be31f59c956b16093d37d2f5359b
junyiguo <junyi.guo@thoughtworks.com>
2023-09-07 15:11:33 +0800

    [34-3][Junyi & Weixiao] delete no used calculate total price and paid price logic in OrderUtils

--- src/main/java/com/example/domain/util/OrderUtils.java
+++ src/main/java/com/example/domain/util/OrderUtils.java
@@ -1,8 +1,5 @@
 package com.example.domain.util;
 
-import com.example.domain.entity.ProductDetail;
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.UUID;
 
 public class OrderUtils {
@@ -10,21 +7,5 @@ public class OrderUtils {
 
   public static String generateOrderId() {
     return UUID.randomUUID().toString();
-  }
-
-  public static BigDecimal calculateTotalPrice(List<ProductDetail> productDetails) {
-    return productDetails.stream()
-        .map(
-            productDetail ->
-                productDetail
-                    .getUnitPrice()
-                    .multiply(BigDecimal.valueOf(productDetail.getQuantity())))
-        .reduce(BigDecimal.ZERO, BigDecimal::add);
-  }
-
-  public static BigDecimal calculatePaidPrice(List<ProductDetail> productDetails) {
-    return productDetails.stream()
-        .map(ProductDetail::calculatePaidPrice)
-        .reduce(BigDecimal.ZERO, BigDecimal::add);
   }
 }
